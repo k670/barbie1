a6dfe2af5ae11f296cec4ec961eb2631052622f2
Alona Melnyk <amelnyk@playtika.com>
2019-10-16 18:51:03 +0300

    .gitignore fix

--- src/main/java/com/courses/barbie/AccountBalanceDiffDTO.java
+++
@@ -1,9 +0,0 @@
-package com.courses.barbie;
-
-import lombok.Data;
-
-@Data
-public class AccountBalanceDiffDTO {
-    private int accountId;
-    private int balanceDifference;
-}
--- src/main/java/com/courses/barbie/dao/ClothesDAO.java
+++
@@ -1,9 +0,0 @@
-package com.courses.barbie.dao;
-
-import com.courses.barbie.entities.ClothesEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClothesDAO extends CrudRepository<ClothesEntity, Integer> {
-}
